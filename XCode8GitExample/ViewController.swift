2c35669cde5b63ef5cb9cf88fa71599579b1ebab
Vanessa Neves <qt_ness2002@hotmail.com>
2018-02-14 08:10:00 -0500

    Initial Commit

---
+++ XCode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCode8GitExample
+//
+//  Created by Vanessa Neves on 2018-02-14.
+//  Copyright © 2018 Vanessa Neves. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ XCode8GitExampleTests/XCode8GitExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  XCode8GitExampleTests.swift
+//  XCode8GitExampleTests
+//
+//  Created by Vanessa Neves on 2018-02-14.
+//  Copyright © 2018 Vanessa Neves. All rights reserved.
+//
+
+import XCTest
+@testable import XCode8GitExample
+
+class XCode8GitExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
